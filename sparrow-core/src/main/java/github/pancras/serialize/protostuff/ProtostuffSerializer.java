775e9cccd27e8d4cbcc0230a3a6cdbc06cc65830
PancrasL <pengzelv@qq.com>
2021-07-12 17:09:16 +0800

    添加了protostuff序列化类

---
+++ sparrow-core/src/main/java/github/pancras/serialize/protostuff/ProtostuffSerializer.java
@@ -0,0 +1,34 @@
+package github.pancras.serialize.protostuff;
+
+import github.pancras.serialize.Serializer;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * @author PancrasL
+ */
+public class ProtostuffSerializer implements Serializer {
+    @Override
+    public byte[] serialize(Object obj) {
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        Class<?> clazz = obj.getClass();
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        byte[] bytes;
+        try {
+            bytes = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } finally {
+            buffer.clear();
+        }
+        return bytes;
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+}
