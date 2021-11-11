2a755432a76e108b12e6de89fbd796178fc9caaf
PancrasL <pengzelv@qq.com>
2021-11-11 17:30:23 +0800

    implementing

---
+++ lightrpc-core/src/main/java/github/pancras/spring/annotation/RpcReference.java
@@ -0,0 +1,27 @@
+package github.pancras.spring.annotation;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+@Inherited
+public @interface RpcReference {
+
+    /**
+     * Service version, default value is empty string
+     */
+    String version() default "";
+
+    /**
+     * Service group, default value is empty string
+     */
+    String group() default "";
+
+}
---
+++ lightrpc-core/src/main/java/github/pancras/spring/annotation/RpcScan.java
@@ -0,0 +1,21 @@
+package github.pancras.spring.annotation;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import github.javaguide.spring.CustomScannerRegistrar;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Import(CustomScannerRegistrar.class)
+@Documented
+public @interface RpcScan {
+
+    String[] basePackage();
+
+}
