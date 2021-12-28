14946c0e4d380d8efe6b05d0a8d9a17384c7e66c
PancrasL <pengzelv@qq.com>
2021-12-28 17:17:38 +0800

    feture: Redis作为注册中心时使用定时拉取方式更新服务地址

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ LightRPC是一款使用Java语言编写，基于Netty和Zookeeper构建的以学
 
 - 支持zookeeper、redis两种注册中心
 
-  - zookeeper监听器，在服务地址变化时会自动更新本地缓存
+  - Zookeeper作为注册中心时使用监听器方式更新服务地址
+
+  - Redis作为注册中心时使用定时任务拉取方式更新服务地址
 
 - 支持java序列化、kryo、protostuff三种序列化机制
 
--- lightrpc-core/src/main/java/github/pancras/registry/redis/RedisRegistryServiceImpl.java
+++ lightrpc-core/src/main/java/github/pancras/registry/redis/RedisRegistryServiceImpl.java
@@ -4,12 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
 
 import github.pancras.registry.RegistryService;
-import github.pancras.registry.redis.util.JedisUtils;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -17,11 +21,19 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisRegistryServiceImpl implements RegistryService {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisRegistryServiceImpl.class);
+    public static final String REDIS_REGISTER_ROOT_PATH = "/LightRPC";
+    private static final ConcurrentHashMap<String, List<String>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService pullService = Executors.newSingleThreadScheduledExecutor();
 
     private final Jedis jedis;
 
     private RedisRegistryServiceImpl(InetSocketAddress socketAddress) {
         this.jedis = buildJedis(socketAddress);
+        // 每30秒更新一次服务地址列表
+        pullService.schedule(() -> SERVICE_ADDRESS_MAP.forEachEntry(5, (e) -> {
+            ArrayList<String> address = new ArrayList<>(jedis.smembers(e.getKey()));
+            e.setValue(address);
+        }), 30, TimeUnit.SECONDS);
     }
 
     public static RedisRegistryServiceImpl newInstance(InetSocketAddress socketAddress) {
@@ -38,19 +50,23 @@ public class RedisRegistryServiceImpl implements RegistryService {
 
     @Override
     public void register(@Nonnull String rpcServiceName, @Nonnull InetSocketAddress address, @Nonnull Integer weight) {
-        String key = JedisUtils.REDIS_REGISTER_ROOT_PATH + "/" + rpcServiceName;
+        String key = REDIS_REGISTER_ROOT_PATH + "/" + rpcServiceName;
         String value = address.toString();
-        JedisUtils.createNode(jedis, key, value);
+        jedis.sadd(key, value);
     }
 
     @Override
     public List<String> lookup(@Nonnull String rpcServiceName) {
-        String key = JedisUtils.REDIS_REGISTER_ROOT_PATH + "/" + rpcServiceName;
-        return JedisUtils.getNodes(jedis, key);
+        String key = REDIS_REGISTER_ROOT_PATH + "/" + rpcServiceName;
+        if (!SERVICE_ADDRESS_MAP.containsKey(key)) {
+            ArrayList<String> address = new ArrayList<>(jedis.smembers(key));
+            SERVICE_ADDRESS_MAP.put(key, address);
+        }
+        return SERVICE_ADDRESS_MAP.get(key);
     }
 
     @Override
     public void close() {
-
+        jedis.close();
     }
 }
