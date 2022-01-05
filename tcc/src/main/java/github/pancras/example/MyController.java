09a6c6b01c00408dc35a8cb0ae23f9266241a702
PancrasL <pengzelv@qq.com>
2022-01-05 15:59:27 +0800

    temp

--- tcc/src/main/java/github/pancras/example/MyController.java
+++ tcc/src/main/java/github/pancras/example/MyController.java
@@ -2,17 +2,7 @@ package github.pancras.example;
 
 import org.springframework.stereotype.Component;
 
-import github.pancras.example.service.BalanceService;
-import github.pancras.example.service.OrderService;
-import github.pancras.example.service.StockService;
-import github.pancras.spring.annotation.RpcReference;
-
 @Component
 public class MyController {
-    @RpcReference
-    private BalanceService balance;
-    @RpcReference
-    private OrderService order;
-    @RpcReference
-    private StockService stock;
+
 }
